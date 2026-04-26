47652f39c4ce127c36719eff2c2ed4e7dfc34e63
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2026-04-26 21:12:20 +0300

    home and shoppingList responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import Layout from "../components/Layout";
 import Categories from "../components/Categories";
 import BookList from "../components/BookList";
+import css from "./Home.module.css";
 
 export default function Home({ selectedCategory, setSelectedCategory }) {
   return (
     <Layout sidebar={<Categories onSelect={setSelectedCategory} />}>
-      <h1>
-        {selectedCategory === "all" ? "Best Sellers Books" : selectedCategory}
-      </h1>
+      <div className={css.wrapper}>
+        <h1 className={css.title}>
+          {selectedCategory === "all" ? "Best Sellers Books" : selectedCategory}
+        </h1>
 
-      <BookList selectedCategory={selectedCategory} />
+        <BookList selectedCategory={selectedCategory} />
+      </div>
     </Layout>
   );
 }
