86a8c20103bad91fb6c48b395ed2af06994a7954
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2026-04-26 21:21:13 +0300

    genel css

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,9 @@ import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ShoppingList from "./pages/ShoppingList";
-import { getTheme, setTheme } from "./utils/theme";
 function App() {
   const [isAuthOpen, setIsAuthOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("all");
-  const [theme, setThemeState] = useState(getTheme());
-
-  useEffect(() => {
-    document.body.setAttribute("data-theme", theme);
-    setTheme(theme);
-  }, [theme]);
 
   
 
--- src/utils/theme.js
+++
@@ -1,9 +0,0 @@
-const THEME_KEY = "theme";
-
-export const getTheme = () => {
-  return localStorage.getItem(THEME_KEY) || "light";
-};
-
-export const setTheme = (theme) => {
-  localStorage.setItem(THEME_KEY, theme);
-};
